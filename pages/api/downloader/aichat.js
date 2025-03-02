6ac124db02ad0e540dc3990825f1c688e9ac207d
ErerexID Chx <luwibuu100@gmail.com>
2025-03-02 08:46:40 +0700

    Update aichat.js

--- pages/api/downloader/aichat.js
+++ pages/api/downloader/aichat.js
@@ -10,10 +10,10 @@ export default async function handler(req, res) {
         });
     }
 
-    const { query } = req.query;
+    const { prompt } = req.query;
     
     try {
-        const data = await robloxStalk(query); // ini error gak ada
+        const data = await Ai4Chat(prompt); // ini error gak ada
         res.status(200).json({
             status: true,
             creator: CREATOR,
