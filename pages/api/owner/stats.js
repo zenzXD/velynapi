0ccbb7fef3fdbd3914c06990afbdc59f803f30fc
ErerexID Chx <luwibuu100@gmail.com>
2025-03-07 04:20:09 +0700

    Create stats.js

---
+++ pages/api/owner/stats.js
@@ -0,0 +1,28 @@
+import { CREATOR } from "../../../settings";
+import { getAllRequestCounts } from "../../../redis";
+
+export default async function handler(req, res) {
+    if (req.method !== "GET") {
+        return res.status(405).json({
+            status: false,
+            creator: CREATOR,
+            error: "Method Not Allowed",
+        });
+    }
+  
+    try {
+        const totalRequest = await getAllRequestCounts();
+        res.status(200).json({
+            status: true,
+            creator: CREATOR,
+            data: data,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: false,
+            creator: CREATOR,
+            error: "Internal Server Error",
+        });
+    }
+}
