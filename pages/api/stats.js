c6e0716339690baf4080378088fa9b03915820a6
ErerexID Chx <luwibuu100@gmail.com>
2025-03-06 23:22:37 +0700

    Create stats.js

---
+++ pages/api/stats.js
@@ -0,0 +1,28 @@
+import [ getAllRequestCounts } from "../../../redis";
+import { API_KEY, CREATOR } from "../../../settings";
+
+export default async function handler(req, res) {
+    if (req.method !== "GET") {
+        return res.status(405).json({
+            status: false,
+            creator: CREATOR,
+            error: "Method Not Allowed",
+        });
+    }
+  
+    try {
+        const totalRequest = await getAllRequestCounts();
+        res.status(200).json({
+            status: true,
+            creator: CREATOR,
+            data: data,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: false,
+            creator: CREATOR,
+            error: "Internal Server Error",
+        });
+    }
+}
