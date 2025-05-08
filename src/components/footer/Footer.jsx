a2501b51d19ddd256acd5602ffddd35bb307ddc3
zenzXD <aaku85019@gmail.com>
2025-05-08 18:19:02 +0700

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Velyn</div>
+      <div className={styles.logo}>zennz</div>
       <div className={styles.text}>
-        VelynTeam © All rights reserved 2025.
+        zennz-api-team © All rights reserved 2025.
       </div>
     </div>
   );
